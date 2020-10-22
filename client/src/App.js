8de4328494dd7280f98f12f12d879ab6293bbc3f
sfazli96 <sfazli96@gmail.com>
2020-10-21 22:46:57 -0700

    Starting NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 class App extends Component {
   constructor(props) {
@@ -21,14 +21,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <p className="App-intro">{this.state.apiResponse}</p>
-          </a>
-        </header>
+         <Navbar></Navbar>
       </div>
     );
   }
