19fb4322d1866b248b2c6903b6441c7112762773
ChairMane <danielschris899>
2020-10-30 13:08:58 -0700

    Deleted React/Added Django

--- api/routes/search.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/sears', function(req, res, next) {
-	console.log(req.query);
-    res.send(['A', 'B', 'C']);
-});
-
-module.exports = router;
