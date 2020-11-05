0ad6b547d0729aafba6e4fd2cb156b47b5219142
sfazli96 <sfazli96@gmail.com>
2020-11-05 03:41:23 -0800

    Added Home picture and working on Checkboxes for Country

--- mysite/client/forms.py
+++ mysite/client/forms.py
@@ -6,3 +6,14 @@ class USForm(forms.Form):
 	publish_date = forms.DateField(required=False, label='Publish Date:')
 	category_id = forms.IntegerField(required=False, label='Category ID:')
 	tags = forms.CharField(required=False, label='Tags:')
+
+
+
+class countryForm(forms.Form):
+	country = forms.CharField(required=True, label='Country')
+	channel_title = forms.CharField(required=False, label='Channel name:')
+	video_id = forms.CharField(required=False, label='Video ID:')
+	publish_date = forms.DateField(required=False, label='Publish Date:')
+	category_id = forms.IntegerField(required=False, label='Category ID:')
+	tags = forms.CharField(required=False, label='Tags:')
+
--- mysite/client/urls.py
+++ mysite/client/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
 	path('US/', views.UnitedStatesView.as_view(), name='US'),
 	path('', views.home, name='home'),
+	path('great_britain/', views.CountryView.as_view(), name='great_britain'),
+	path('canada/', views.canadaView.as_view(), name='canada'),
+	path('country/', views.country, name='country'),
+	path('action_page/', views.action_page, name='action'),
+
 ]
